5374728f686b5542d2e78d6fa88b3a10383e99f0
josue1dario2 <josue1dario2solis3@gmail.com>
2025-06-02 01:34:07 -0300

    resolver conflicto

--- funciones.h
+++ funciones.h
@@ -1,16 +1,5 @@
 #pragma once
-#ifndef FUNCIONES_H
-#define FUNCIONES_H
 
 int seleccionarOpcion();
-<<<<<<< HEAD
 void limpiarPantalla();
 void mostrarCreditos();
-=======
-
-void mostrarCreditos();
-
-void limpiarPantalla();
-
-#endif
->>>>>>> 384898d8dea2760fd7b31318303d481288033803
