26983aa9cbd7034efaf8794fe9258abd050c5638
josue1dario2 <josue1dario2solis3@gmail.com>
2025-06-03 23:14:22 -0300

    agregar la fucion elegirCombinacion

--- funciones.cpp
+++ funciones.cpp
@@ -26,7 +26,6 @@ int seleccionarOpcion()
     return opcion;
 }
 
-
 int tirarDado(int caras)
 {
     return (rand() % caras) + 1;
@@ -81,20 +80,65 @@ int quienEmpieza(string jugador1, string jugador2)
         return 2;
     }
 }
-int lanzarDadosObjetivo(){ 
+int lanzarDadosObjetivo()
+{
     return tirarDado(12) + tirarDado(12);
 }
-void tirarDadosJugador(int cantidad,int dados[]){
+void tirarDadosJugador(int cantidad, int dados[])
+{
 
-for (int i=0; i<cantidad; i++){
-    dados[i]=tirarDado(cantidad);
-}
+    for (int i = 0; i < cantidad; i++)
+    {
+        dados[i] = tirarDado(cantidad);
+    }
 }
 
-int computarPuntaje(int suma, int dadosElegidos[], int tamano, int objetivo) {
-    if (suma == objetivo) {
+int computarPuntaje(int suma, int dadosElegidos[], int tamano, int objetivo)
+{
+    if (suma == objetivo)
+    {
         return suma * tamano;
-    } else {
+    }
+    else
+    {
         return 0;
+    }
+}
+
+void elegirCombinacion(int objetivo, int dados[], int tamano, int seleccionados[], int &tamanoSeleccionados)
+{
+    tamanoSeleccionados = 0;
+    int sumaActual = 0;
+    cout << "Dados disponibles:\n";
+    for (int i = 0; i < tamano; i++)
+    {
+        cout << "  [" << i + 1 << "] " << dados[i];
+    }
+    cout << "\n\nSelecciona los dados por su número (0 para terminar): ";
+    while (sumaActual < objetivo)
+    {
+        int indiceDado;
+        cin >> indiceDado;
+        if (indiceDado == 0)
+        {
+            tamanoSeleccionados = 0;
+            return;
+        }
+        if (indiceDado < 1 || indiceDado > tamano)
+        {
+            cout << "Número inválido. Intenta de nuevo: ";
+            continue;
+        }
+        seleccionados[tamanoSeleccionados++] = dados[indiceDado - 1];
+        sumaActual += dados[indiceDado - 1];
+        cout << "Suma actual: " << sumaActual << "\n";
+        if (sumaActual == objetivo)
+            return;
+        if (sumaActual > objetivo)
+        {
+            tamanoSeleccionados = 0;
+            return;
+        }
+        cout << "Siguiente dado (0 para terminar): ";
     }
 }
--- funciones.h
+++ funciones.h
@@ -6,5 +6,6 @@ void mostrarCreditos();
 int tirarDado(int caras);
 int quienEmpieza(string nombre1, string nombre2);
 int lanzarDadosObjetivo();
-void tirarDadosJugador(int cantidad,int dados[]);
+void tirarDadosJugador(int cantidad, int dados[]);
 int computarPuntaje(int suma, int dadosElegidos[], int tamano, int objetivo);
+void elegirCombinacion(int objetivo, int dados[], int tamano, int seleccionados[], int &tamanoSeleccionados);
