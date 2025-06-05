9ca9070ab1a7a5c39db0ed9df2403b8a5e0255a8
cami9553 <camilarivero1997@outlook.com>
2025-06-05 15:52:23 -0300

    arreglar

--- funciones.cpp
+++ funciones.cpp
@@ -153,7 +153,7 @@ void elegirCombinacion(int objetivo, int dados[], int tamano, int seleccionados[
         }
         cout << "Siguiente dado (0 para terminar): ";
     }
-
+}
 void modificarDados(int &jugador, int tamanoSeleccionados, int &rival)
 {
     jugador -= tamanoSeleccionados;
