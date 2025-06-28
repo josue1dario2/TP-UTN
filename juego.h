47be819f3ee2a65acebfc32513c57d9a35cd1106
josue1dario2 <josue1dario2solis3@gmail.com>
2025-06-28 20:59:51 -0300

    agregar main.h

--- juego.h
+++ juego.h
@@ -1,11 +1,5 @@
 #pragma once
 
-#include "dados.h"
-#include "interfaz.h"
-#include "puntos.h"
-#include "stocks.h"
-#include "turnos.h"
-
 #include <string>
 using namespace std;
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <time.h>
-#include "juego.h"
+#include "main.h"
 using namespace std;
 
 int main()
